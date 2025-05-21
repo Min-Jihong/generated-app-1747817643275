d7442b7a5d6d914027da196de1f3ed36925d4298
Min-Jihong <69140228+Min-Jihong@users.noreply.github.com>
2025-05-21 17:54:07 +0900

    Add src/components/ProductDetailPage.js

---
+++ src/components/ProductDetailPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function ProductDetailPage() {
+  return (
+    <div className='container mx-auto p-4'>
+      <h1 className='text-xl font-bold'>Product Detail</h1>
+      {/* Product detail content goes here */}
+    </div>
+  );
+}
